93a14d322403f821f8e930a9244121b910cf73c5
chrisala <chris.godwin.ala@gmail.com>
2016-04-14 09:43:03 +1000

    Extracted files from Biocollect.

---
+++ grails-app/conf/DocumentPreviewResources.groovy
@@ -0,0 +1,9 @@
+modules = {
+
+    leaflet {
+        dependsOn 'jquery'
+        resource url: 'vendor/leaflet/0.7.3/leaflet.css'
+        resource url: 'vendor/leaflet/0.7.3/leaflet.js'
+    }
+
+}
