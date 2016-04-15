76eb8df481ce527812d88101b6056fd803cfd0e7
temi <temivarg@gmail.com>
2016-04-15 10:50:19 +1000

    changing module name to avoid conflict.

--- grails-app/conf/DocumentPreviewResources.groovy
+++ grails-app/conf/DocumentPreviewResources.groovy
@@ -1,6 +1,6 @@
 modules = {
 
-    leaflet {
+    resourceLeaflet {
         dependsOn 'jquery'
         resource url: 'vendor/leaflet/0.7.3/leaflet.css'
         resource url: 'vendor/leaflet/0.7.3/leaflet.js'
